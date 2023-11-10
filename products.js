67270a0b28e817f797f01b85e3ce4dbbdaa25b70
luki399 <150478438+luki399@users.noreply.github.com>
2023-11-10 20:35:44 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,79 @@
+const products = [{
+        id: 0,
+        name: "Iphone 15 Pro",
+        description: `Smartfon APPLE iPhone 15 Pro 128GB 5G 6.1" 120Hz Tytan błękitny`,
+        category: "Iphone",
+        price: 5999.99,
+        image: "./zdjecia/ip15pro128gb.jpg",
+    },
+    {
+        id: 1,
+        name: "Iphone 15",
+        description: `
+    Smartfon APPLE iPhone 15 128GB 5G 6.1" Czarny`,
+        category: "Iphone",
+        price: 4499.99,
+        image: "./zdjecia/ip15128gb.jpg",
+        sale: true,
+        saleAmount: 200.,
+    },
+    {
+        id: 2,
+        name: "Samsung S23 Ultra",
+        description: `
+    Smartfon SAMSUNG Galaxy S23 Ultra 8/256GB 5G 6.8" 120Hz Czarny SM-S918`,
+        category: "Samsung",
+        price: 5799.99,
+        image: "./zdjecia/s23ultra.jpg",
+    },
+    {
+        id: 3,
+        name: "Samsung S23",
+        description: `
+    Smartfon SAMSUNG Galaxy S23 8/128GB 5G 6.1" 120Hz Czarny SM-S911`,
+        category: "Samsung",
+        price: 3999.99,
+        image: "./zdjecia/s23.jpg",
+        sale: true,
+        saleAmount: 40
+    },
+    {
+        id: 4,
+        name: "Motorola Edge 30 Neo",
+        description: `
+    Smartfon MOTOROLA Edge 30 Neo 8/128GB 5G 6.28" 120Hz Czarny PAV00004PL`,
+        category: "Motorola",
+        price: 989.99,
+        image: "./zdjecia/edge30.jpg",
+    },
+    {
+        id: 5,
+        name: "Motorola G32",
+        description: `
+    Smartfon MOTOROLA Moto G32 8/256GB 6.5" 90Hz Szary PAUU0047PL`,
+        category: "Motorola",
+        price: 799,
+        image: "./zdjecia/g32.jpg",
+    },
+    {
+        id: 6,
+        name: "Samsung S22",
+        description: `
+    Smartfon SAMSUNG Galaxy S22 8/128GB 5G 6.1" 120 Hz Czarny SM-S901`,
+        category: "Samsung",
+        price: 2799,
+        image: "./zdjecia/s22.jpg",
+        sale: true,
+        saleAmount: 100
+    },
+    {
+        id: 7,
+        name: "Oppo Find N2 Flip",
+        description: `
+    Smartfon OPPO Find N2 Flip 8/256GB 5G 6.8" 120Hz Czarny`,
+        category: "Oppo",
+        price: 4999.99,
+        image: "./zdjecia/n2.jpg",
+    },
+
+];
